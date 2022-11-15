55bdad56cb9aef313cecb9e1a2f50c0bb35478fa
Udor Blessingm <berachah54@gmail.com>
2022-11-15 10:46:35 +0100

    add shipping amount

--- public/js/rkfl_checkout.js
+++ public/js/rkfl_checkout.js
@@ -1,14 +1,15 @@
 (() => {
-    var serverApiUrl = 'https://bigcommerce.rocketfuelblockchain.com';
-    
-     var path = window.location.pathname; let currentPage = path.split('/').pop(); var thisScript = document.currentScript; function paramsToJSON(search) { search = search.substring(1); return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}') }
+    // var serverApiUrl = 'https://bigcommerce.rocketfuelblockchain.com';
+    var serverApiUrl = 'https://3231-102-89-22-234.ngrok.io';
+
+    var path = window.location.pathname; let currentPage = path.split('/').pop(); var thisScript = document.currentScript; function paramsToJSON(search) { search = search.substring(1); return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}') }
     var RocketfuelPaymentEngine = {
         order_id: '', url: new URL(window.location.href), hash: '', watchIframeShow: false, rkflConfig: null, buttonEventAdded: false, user_data: { email: null }, loading: false, response: { uuid: '', temporary_order_id: '' }, getEnvironment: function () { return RocketfuelPaymentEngine.response?.environment || 'prod'; }, getUserData: function () {
             let email = document.querySelector('.customerView-body.optimizedCheckout-contentPrimary')?.innerText || null; let user_data = { firstName: document.querySelector('.first-name')?.innerText || null, lastName: document.querySelector('.family-name')?.innerText || null, email, }
             if (!user_data) return false; return user_data;
         }, triggerPlaceOrder: function () { document.getElementById('checkout-payment-continue').disabled = false; document.getElementById('checkout-payment-continue').click(); }, updateOrder: function (result) {
             try {
-                 document.getElementById('checkout-payment-continue').dataset.rkfl = 'notactive'
+                document.getElementById('checkout-payment-continue').dataset.rkfl = 'notactive'
             } catch (error) { console.error('Error from update order method', error?.message); }
         }, startPayment: function (autoTriggerState = true) { this.watchIframeShow = true; let checkIframe = setInterval(() => { if (RocketfuelPaymentEngine.rkfl.iframeInfo.iframe) { RocketfuelPaymentEngine.rkfl.initPayment(); clearInterval(checkIframe); } }, 500); }, prepareRetrigger: function () {
             if (document.getElementById('rocketfuel_overlay_page_for_iframe')) { document.getElementById('rocketfuel_overlay_page_for_iframe').remove(); }
@@ -49,21 +50,47 @@
             console.log('Done initiating RKFL'); engine.windowListener(); engine.startPayment();
         }
     }
+    function getcheckoutTotal(cart, shipping) {
+        if (!shipping) {
+            return cart
+        }
+        return parseFloat(cart.toString()) + parseFloat(shipping.toString())
+    }
+    function sortShipping(amount) {
+        return { 'name': 'Shipping', 'id': 'shipping', 'price': amount, 'quantity': '1' }
+    }
     function sortIndividual(items) { return items.map(item => { return { 'name': item.name, 'id': item.id.toString(), 'price': item.listPrice, 'quantity': item.quantity.toString() } }) }
-    function sortCart(cartItems) {
-        let customItems = digitalItems = giftCertificates = physicalItems = []; if (cartItems.customItems.length > 0) { customItems = sortIndividual(cartItems.customItems); }
+    function sortCart({ cart: cartItems, shipping }) {
+        let customItems = shippingItems = digitalItems = giftCertificates = physicalItems = []; if (cartItems.customItems.length > 0) { customItems = sortIndividual(cartItems.customItems); }
         if (cartItems.digitalItems.length > 0) { digitalItems = sortIndividual(cartItems.digitalItems); }
         if (cartItems.giftCertificates.length > 0) { giftCertificates = sortIndividual(cartItems.giftCertificates); }
         if (cartItems.physicalItems.length > 0) { physicalItems = sortIndividual(cartItems.physicalItems); }
-        return [...customItems, ...digitalItems, ...giftCertificates, ...physicalItems]
+        if (shipping?.amount && parseFloat(shipping.amount) > 0) {
+            shippingItems = [sortShipping(shipping.amount)]
+        }
+        return [...customItems, ...digitalItems, ...giftCertificates, ...physicalItems, ...shippingItems]
     }
     async function initUUID() {
         var requestOptions = { method: 'GET', redirect: 'follow' }; try {
-            let rootUrl = window.location.origin; let response = await fetch(rootUrl + "/api/storefront/carts", requestOptions); let result = await response.text(); result = JSON.parse(result); let theIndex = result[0]; let cart; if (theIndex && theIndex.id) {
-                RocketfuelPaymentEngine.user_data.email = theIndex.email; cart = sortCart(theIndex.lineItems); let payload = { currency: theIndex.currency.code, cart, amount: theIndex.baseAmount, storeHash: RocketfuelPaymentEngine.hash }
-                var myHeaders = new Headers(); myHeaders.append("Content-Type", "application/json"); var requestOptions = { method: 'POST', headers: myHeaders, body: JSON.stringify(payload), }; let uuidResponse = await fetch(serverApiUrl + "/api/payment", requestOptions); let uuidResult = await uuidResponse.json(); 
+            let rootUrl = window.location.origin; let response = await fetch(rootUrl + "/api/storefront/carts", requestOptions);
+
+            let result = await response.text(); result = JSON.parse(result); let theIndex = result[0]; let cart, checkoutResult; if (theIndex && theIndex.id) {
+                try {
+                    let checkoutResponse = await fetch(rootUrl + "/api/storefront/checkout/" + theIndex.id, requestOptions);
+                    checkoutResult = await checkoutResponse.text(); checkoutResult = JSON.parse(checkoutResult);
+
+
+                } catch (error) {
+                    console.error('[COULDNOT_SHIPPING]', error)
+                }
+
+
+                RocketfuelPaymentEngine.user_data.email = theIndex.email; cart = sortCart({ cart: theIndex.lineItems, shipping: { amount: checkoutResult?.shippingCostTotal } }); let payload = { currency: theIndex.currency.code, cart, amount: getcheckoutTotal(theIndex.baseAmount,checkoutResult?.shippingCostTotal) }
+                console.warn('[DATA_GENERATED]', { payload });
+                payload = {...payload,storeHash: RocketfuelPaymentEngine.hash}
+                var myHeaders = new Headers(); myHeaders.append("Content-Type", "application/json"); var requestOptions = { method: 'POST', headers: myHeaders, body: JSON.stringify(payload) }; let uuidResponse = await fetch(serverApiUrl + "/api/payment", requestOptions); let uuidResult = await uuidResponse.json();
                 RocketfuelPaymentEngine.response = uuidResult
-                
+
                 RocketfuelPaymentEngine.setLocalStorage('merchant_auth_rocketfuel', RocketfuelPaymentEngine.response.merchantAuth)
                 RocketfuelPaymentEngine.setLocalStorage('temp_orderid_rocketfuel', RocketfuelPaymentEngine.response.temporaryOrderId)
                 RocketfuelPaymentEngine.setLocalStorage('uuid_rocketfuel', RocketfuelPaymentEngine.response.uuid)
@@ -82,9 +109,9 @@
         RocketfuelPaymentEngine.init();
     }
     function handleClick(e) {
-      
-        const excludedIds = ['terms','rkfl-btn-place-order','checkout-payment-continue'];
-      
+
+        const excludedIds = ['terms', 'rkfl-btn-place-order', 'checkout-payment-continue'];
+
         if (!e.target?.id || excludedIds.includes(e.target.id)) { return }
         if (e.target.id === 'radio-moneyorder') {
             if (!document.getElementById('rkfl-btn-place-order')) { createPlaceOrderButton() }
@@ -120,9 +147,9 @@
         })
     }
     if (currentPage.includes('checkout')) {
-        localStorage.removeItem('temp_orderid_rocketfuel'); 
-        localStorage.removeItem('merchant_auth_rocketfuel'); 
-         function createPlaceOrderButton() {
+        localStorage.removeItem('temp_orderid_rocketfuel');
+        localStorage.removeItem('merchant_auth_rocketfuel');
+        function createPlaceOrderButton() {
             if (document.getElementById('rkfl-btn-place-order')) { return; }
             const rfklCheckoutBtnCover = document.createElement('div'); rfklCheckoutBtnCover.classList.add('form-actions'); const rfklCheckoutBtn = document.createElement('div'); rfklCheckoutBtn.id = 'rkfl-btn-place-order'; rfklCheckoutBtn.disabled = true; rfklCheckoutBtn.style.cssText = 'display:none;background-color:#333;border-color:#333;color:#fff;font-family:Montserrat,Arial,Helvetica,sans-serif;padding:1.1875rem 4.5rem;font-size:1.38462rem;text-align:center;cursor:pointer;margin-left:0;width:100%'; rfklCheckoutBtn.addEventListener('click', handlePlaceOrderButton); rfklCheckoutBtn.innerHTML = 'PLACE ORDER WITH ROCKETFUEL'; rfklCheckoutBtnCover.appendChild(rfklCheckoutBtn)
             const checkoutPayIn = setInterval(() => { if (document.getElementById('checkout-payment-continue')) { document.getElementById('checkout-payment-continue').parentNode.parentNode.insertBefore(rfklCheckoutBtnCover, document.getElementById('checkout-payment-continue').parentNode); clearInterval(checkoutPayIn); } }, 1000);
@@ -150,19 +177,21 @@
         });
     }
     if (currentPage.includes('order-confirmation')) {
- 
-        let uuid = localStorage.getItem('uuid_rocketfuel'); 
-        let merchant_auth_rocketfuel = localStorage.getItem('merchant_auth_rocketfuel'); 
-        
+
+        let uuid = localStorage.getItem('uuid_rocketfuel');
+        let merchant_auth_rocketfuel = localStorage.getItem('merchant_auth_rocketfuel');
+
         async function sortSuccessPage() {
-            if ( uuid ) {
+            if (uuid) {
                 const storeHash = await getStoreHash(); let thankyouInter = setInterval(async () => {
                     if (!document.querySelector('p[data-test=order-confirmation-order-status-text]')) return; document.querySelector('p[data-test=order-confirmation-order-status-text]').innerHTML = 'Your order has been received, An email will be sent containing information about your purchase.'; document.querySelector('div[data-test=payment-instructions]').innerHTML = ''; clearInterval(thankyouInter); try {
                         const temp_orderid_rocketfuel = localStorage.getItem("temp_orderid_rocketfuel"); const order_id = document.querySelector('p[data-test=order-confirmation-order-number-text] strong').innerText; if (!order_id && !temp_orderid_rocketfuel) { return; }
                         var myHeaders = new Headers(); myHeaders.append("Content-Type", "application/json"); myHeaders.append("merchant-auth", merchant_auth_rocketfuel);
-                        
-                        const payload = { temporaryOrderId: temp_orderid_rocketfuel, orderId: order_id, storeHash: RocketfuelPaymentEngine.hash,uuid }
-                        var requestOptions = { method: 'POST', headers: myHeaders, body: JSON.stringify(payload), }; const result = await fetch(serverApiUrl + "/api/sort-order", requestOptions); console.log("Result from swap", { result });
+
+                        const payload = { temporaryOrderId: temp_orderid_rocketfuel, orderId: order_id, storeHash: RocketfuelPaymentEngine.hash, uuid }
+                        var requestOptions = { method: 'POST', headers: myHeaders, body: JSON.stringify(payload), }; const response = await fetch(serverApiUrl + "/api/sort-order", requestOptions);
+                        const result = await response.json()
+                        console.log("Result from swap", { result });
                     } catch (error) { console.error(error?.message); }
                 }, 500);
             }
@@ -170,3 +199,4 @@
         sortSuccessPage();
     }
 })();
+console.log('v1.0.1')
